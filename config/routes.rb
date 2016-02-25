afcfffdb99d6a5c0359f255cb0a9bcc2c19670b2
Jincheng Zhang <zjc1996@brandeis.edu>
2016-02-24 20:20:35 -0500

    sync

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'dashboard/index'
+
   get 'welcome/index'
 
   resources :interests
@@ -9,7 +11,7 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root 'welcome#index'
+  root 'dashboard#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
