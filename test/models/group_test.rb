80c3b93e0ee9a0611c21866d8facabb85f5805c1
zhengyr <stella.zhengyr@gmail.com>
2016-03-02 04:05:23 -0500

    minitest for group and homework

--- test/models/group_test.rb
+++ test/models/group_test.rb
@@ -1,7 +1,31 @@
 require 'test_helper'
 
 class GroupTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  it "has two groups in database" do
+    Group.count.must_equal 2
+  end
+
+  it "can accept a new group" do
+    Group.create(name: "G3")
+    Group.count.must_equal 3
+  end
+
+  it "can delete an existing group" do
+    g = Group.create(name: "G3")
+    Group.count.must_equal 3
+    n = g.id
+    Group.delete(n)
+    Group.count.must_equal 2
+  end
+
+  it "cannot save a hw without the a name" do
+    w = Group.create()
+    w.valid?.must_equal false
+  end
+
+  it "group name has to be unique" do
+    g = Group.create(name: "G2")
+    assert_not g.valid?
+  end
+
 end
--- test/models/homework_test.rb
+++ test/models/homework_test.rb
@@ -1,7 +1,28 @@
 require 'test_helper'
 
 class HomeworkTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  it "has two hws in database" do
+    Homework.count.must_equal 2
+  end
+
+  it "can accept a new hw" do
+    Homework.create(name: "HW3", due_date: "2016-01-24 20:59:29")
+    Homework.count.must_equal 3
+  end
+  
+  it "can delete an existing hw" do
+    h = Homework.create(name: "Hw3", due_date: "2016-01-24 20:59:29")
+    Homework.count.must_equal 3
+    n = h.id
+    Homework.delete(n)
+    Homework.count.must_equal 2
+  end
+
+  it "cannot save a hw without the a name or due_date" do
+    w = Homework.create(name: "hw4")
+    w.valid?.must_equal false
+    w = Homework.create(due_date: "2016-01-24 20:59:29")
+    w.valid?.must_equal false
+  end
+
 end
