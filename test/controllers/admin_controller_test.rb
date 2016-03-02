9a99febb4b3aa960dd7a89ac7ee44394c815f6d8
Jincheng Zhang <zjc1996@brandeis.edu>
2016-03-02 13:58:54 -0500

    admin test

--- test/controllers/admin_controller_test.rb
+++ test/controllers/admin_controller_test.rb
@@ -15,12 +15,14 @@ class AdminControllerTest < ActionController::TestCase
   end
 
   test "protected admin page success" do
-      dave = 
+      login_as(:one)
       get :index
-      unless session[:user_id]
-          assert_redirected_to login_url
-      end
       assert_response :success
   end
 
+  test "protected admin page failure" do
+      session[:user_id] = nil
+      get :index
+      assert_redirected_to login_url
+  end
 end
