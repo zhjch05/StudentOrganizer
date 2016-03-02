7ddcab6ad28061a20d244e38b802f1ca04a7f708
Jincheng Zhang <zjc1996@brandeis.edu>
2016-03-02 13:47:53 -0500

    fix destroy

--- test/controllers/admin_controller_test.rb
+++ test/controllers/admin_controller_test.rb
@@ -6,4 +6,21 @@ class AdminControllerTest < ActionController::TestCase
     assert_response :success
   end
 
+  test "protected admin page" do
+      get :index
+      unless session[:user_id]
+          assert_redirected_to login_url
+      end
+      assert_response :success
+  end
+
+  test "protected admin page success" do
+      dave = 
+      get :index
+      unless session[:user_id]
+          assert_redirected_to login_url
+      end
+      assert_response :success
+  end
+
 end
