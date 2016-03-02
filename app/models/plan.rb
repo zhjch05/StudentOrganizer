710f86cbc8a30fee314047d0a7253a1e6bf7e5da
XingyuLu <rmsylar@gmail.com>
2016-03-02 15:46:41 -0500

    sylar 3 tests

--- app/models/plan.rb
+++ app/models/plan.rb
@@ -1,3 +1,5 @@
 class Plan < ActiveRecord::Base
+  validates :name, :user_id, presence: true
+  validates :estimated_time, :presence => true, :if => Proc.new { |a| a.term == "short" }
   belongs_to :user
 end
--- test/models/plan_test.rb
+++ test/models/plan_test.rb
@@ -4,4 +4,29 @@ class PlanTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+
+  describe Plan do
+    it "succeeds in default test" do
+      1.must_equal 1
+    end
+
+    it "must has a name and belongs to a user" do
+      p = Plan.create(user_id: 1)
+      p.valid?.must_equal false
+
+      p2 = Plan.create(name: "Shopping")
+      p2.valid?.must_equal false
+    end
+
+    it "has two plans in database" do
+      Plan.count.must_equal 2
+    end
+
+    it "must has an estimated time if it is short-term" do
+      p = Plan.create(name: "Shopping", user_id: 1, term: "short")
+      p.valid?.must_equal false
+    end
+  end
+  
+
 end
